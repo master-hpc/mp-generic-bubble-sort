7bca0c4af34bf7e1f136b8badfe91b4682f62a93
AnixPasBesoin <anis.lounis@outlook.com>
2018-12-15 01:00:32 +0100

    Wrote the generic version

--- generic-bubble-sort.cu
+++ generic-bubble-sort.cu
@@ -1,11 +1,30 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cuda.h>
+#include <device_functions.h>
+#include <cuda_runtime_api.h>
+
 #include <iostream>
 
-__global__ void bubbleSort(int *v, const int n);
+template<typename T>
+struct ShouldSwap
+{
+	__host__ __device__
+	virtual bool operator() (const T left, const T right) const;
+};
+
+template <typename T>
+__host__ __device__ __inline__
+void swap(T* a, T* r);
+
+template<typename T>
+__global__
+void bubbleSort(T* v, const unsigned int n, ShouldSwap<T> shouldSwap);
 
 int main(int argc, char** argv)
 {
 	// vector size
-	const int size = 10;
+	const unsigned int size = 10;
 
 	// host vector
 	int h_v[size] = { 3, 7, 1, 10, 6, 9, 5, 2, 8, 4 };
@@ -20,37 +39,61 @@ int main(int argc, char** argv)
 	dim3 grdDim(1, 1, 1);
 	dim3 blkDim(size / 2, 1, 1);
 
-	bubbleSort <<<grdDim, blkDim>>>(d_v, size);
+	ShouldSwap<int> shouldSwap;
 
+	bubbleSort<int> <<<grdDim, blkDim >>>(d_v, size, shouldSwap);
+	cudaDeviceSynchronize();
+
+	cudaError_t error = cudaGetLastError();
+	if (error != cudaSuccess) {
+		printf("CUDA error: %s\n", cudaGetErrorString(error));
+		// system("pause"); // when using VisStudio
+		exit(-1);
+	}
+	
 	cudaMemcpy(h_v, d_v, size * sizeof(int), cudaMemcpyDeviceToHost);
-
 	cudaFree(d_v);
 
 	for (int i = 0; i < size; i++) {
-		std::cout << (i == 0 ? "{" : "") <<  h_v[i] << (i < size -1 ? " ," : "}");
+		std::cout << (i == 0 ? "{" : "") << h_v[i] << (i < size - 1 ? " ," : "}");
 	}
 	std::cout << std::endl;
+
+	// system("pause"); // when using VisStudio
 	return 0;
 }
 
-__global__ void bubbleSort(int *v, const int n)
-{
-	int tIdx = threadIdx.x;
+template<typename T>
+__host__ __device__
+bool ShouldSwap<T>::operator() (const T left, const T right) const {
+	return left > right;
+}
 
-        for (int i = 0; i < n; i++) {
 
-		int offset = i % 2;
-		int indiceGauche = 2 * tIdx + offset;
-		int indiceDroite = indiceGauche + 1;
+template<typename T>
+__host__ __device__ __inline__
+void swap (T* a, T* b) {
+	T tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
-                if (indiceDroite < n) {
-			if (v[indiceGauche] > v[indiceDroite]) {
-				int tmp = v[indiceGauche];
-                        	v[indiceGauche] = v[indiceDroite];
-                        	v[indiceDroite] = tmp;
+template<typename T>
+__global__
+void bubbleSort(T* v, const unsigned int n, ShouldSwap<T> shouldSwap) {
+	const unsigned int tIdx = threadIdx.x;
+
+	for (unsigned int i = 0; i < n; i++) {
+
+		unsigned int offset = i % 2;
+		unsigned int indiceGauche = 2 * tIdx + offset;
+		unsigned int indiceDroite = indiceGauche + 1;
+
+		if (indiceDroite < n) {
+			if (shouldSwap(v[indiceGauche], v[indiceDroite])) {
+				swap<T>(&v[indiceGauche], &v[indiceDroite]);
 			}
-                }
+		}
 		__syncthreads();
-
-        }
+	}
 }
